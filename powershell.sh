aa17f3c815eb9dc15c2d5299477c5c6d0cebc682
pentestblog <70894330+pentestblogin@users.noreply.github.com>
2020-11-17 13:24:06 +0530

    Add files via upload

---
+++ powershell.sh
@@ -0,0 +1,13 @@
+#!/bin/bash/
+
+apt update && apt -y install curl gnupg apt-transport-https 
+
+curl https://packages.microsoft.com/keys/microsoft.asc | apt-key add -
+
+echo "deb [arch=amd64] https://packages.microsoft.com/repos/microsoft-debian-stretch-prod stretch main" > /etc/apt/sources.list.d/powershell.list
+
+apt update
+
+apt -y install powershell
+
+
